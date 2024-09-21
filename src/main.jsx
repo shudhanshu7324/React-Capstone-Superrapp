ba61ceac69a42dc40a577c122a711e47224ca945
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-09-21 15:02:26 +0530

    routes setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,20 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Info from "./pages/Info";
+import Browse from "./pages/Browse";
+import NotFound from "./pages/NotFound";
+import Selection from "./pages/Selection";
 
-createRoot(document.getElementById('root')).render(
-  
-    <App />
-)
+createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/info" element={<Info />} />
+      <Route path="/browse" element={<Browse />} />
+      <Route path="/selection" element={<Selection/>} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </Router>
+);
