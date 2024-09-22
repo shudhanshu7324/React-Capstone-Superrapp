d91153d5b45bba1a8c6fda237515d6990956d9f7
Shudhanshu Shree <83982549+shudhanshu7324@users.noreply.github.com>
2024-09-22 22:37:30 +0530

    add: homepage design complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React from "react";
+import "../Css/Home.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+    <div className="home-page">
+      <div className="left">
+        <div className="left-heading">Discover new things on Superapp</div>
+      </div>
+      <div className="right">
+        <div className="right-wrapper">
+          <div className="right-head">
+            <p className="right-heading">Super app</p>
+            <p className="right-subheading">Create your new account</p>
+          </div>
+          <div className="right-form">
+            <form>
+              <input type="text" placeholder="Name" />
+              <span className="error"></span>
+              <input type="text" placeholder="Username" />
+              <span className="error"></span>
 
-export default Home
+              <input type="email" placeholder="Email" />
+              <span className="error"></span>
+
+              <input type="text" placeholder="Mobile" />
+              <span className="error"></span>
+
+              <span className="check">
+                <input type="checkbox" name="check-box" />
+                <label htmlFor="check-box">
+                  Share my registration data with Superapp
+                </label>
+              </span>
+              <span className="error"></span>
+
+              <button className="sign-up-btn">SIGN UP</button>
+            </form>
+          </div>
+          <div className="right-bottom">
+            <p>
+              By clicking on Sign up. you agree to Superapp{" "}
+              <span className="green-text">Terms and Conditions of Use</span>
+            </p>
+            <p>
+              To learn more about how Superapp collects, uses, shares and
+              protects your personal data please head Superapp{" "}
+              <span className="green-text">Privacy Policy</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
